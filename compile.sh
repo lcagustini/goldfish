69da9e33818c0ca8da9bc526a32783194dbc5bb7
lcagustini <lucas.c.agustini@gmail.com>
2023-01-09 21:41:38 -0300

    compile.sh can now create zipped builds that can run outside of mingw

--- compile.sh
+++ compile.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p build
 cd build && cmake .. && cmake --build . && cd ..
 
 DLLS=`ldd ./build/psvita-opengl.exe | grep ucrt64 | cut -d " " -f 3`
 
-if [ -z "$DLLS" ]
+#if [ -z "$DLLS" ]
+#then
+#      echo "DLLs already copied"
+#else
+#      echo "Copying DLLs..."
+#      cp $DLLS build/
+#fi
+
+if [[ $* == *--zip* ]]
 then
-      echo "DLLs already copied"
-else
-      echo "Copying DLLs..."
-      cp $DLLS build/
+    zip -j ./build/standalone.zip $DLLS ./build/psvita-opengl.exe
+    zip -r ./build/standalone.zip assets/
 fi
